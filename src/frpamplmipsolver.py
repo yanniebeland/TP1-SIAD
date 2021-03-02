ffab9c37cc0750c1b54794658718b4e33412897a
Charles Asselin <charles@asselin.xyz>
2021-03-02 13:10:58 -0500

    Complemented frpamplmipsolver class

--- src/frpamplmipsolver.py
+++ src/frpamplmipsolver.py
@@ -9,7 +9,7 @@ import sys
 class FrpAmplMipSolver(solver.Solver):
 
     def __init__(self, prob=frp.FastRouteProb()):
-
+        self.prob = prob
         pass
 
     def solve(self):
@@ -19,8 +19,20 @@ class FrpAmplMipSolver(solver.Solver):
 
             ampl.setOption('solver', 'gurobi')
 
-            model_dir = os.path.normpath('./tp1_ampl')
+            model_dir = os.path.normpath('./ampl_models')
             ampl.read(os.path.join(model_dir, 'Question2.mod'))
+            ampl.setData(self, prob)
+            ampl.solve()
+
+            print('Objective: {}'.format(ampl.getObjective('Total_Cost').value()))
+            solution = ampl.getVariable('Buy').getValues()
+            print('Solution: \n' + str(solution))
+
+        except Exception as e:
+            print(e)
+            raise
+
+
 
 
 
